b418568744babdb623c7b5515352c154669ee0ae
Ian Luca <ianlucabsb@gmail.com>
2022-08-23 18:05:16 -0300

    Busca nota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDeNotasComponent } from './pages/lista-de-notas/lista-de-notas.component';
@@ -20,7 +20,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/lista-de-notas/lista-de-notas.component.ts
+++ src/app/pages/lista-de-notas/lista-de-notas.component.ts
@@ -1,3 +1,4 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { NotasService } from 'src/app/shared/notas.service';
 import { Notas } from 'src/app/shared/note.model';
@@ -5,20 +6,93 @@ import { Notas } from 'src/app/shared/note.model';
 @Component({
   selector: 'app-lista-de-notas',
   templateUrl: './lista-de-notas.component.html',
-  styleUrls: ['./lista-de-notas.component.scss']
+  styleUrls: ['./lista-de-notas.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': '0',
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingRight: '*',
+          paddingLeft: '*',
+        })),
+        animate(68)
+      ]),
+
+      transition('* => void', [
+        animate(50, style({
+          transform: 'scale(0.85)',
+        })),
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75,
+        })),
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0,
+        })),
+        animate('150ms ease-out', style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+          'margin-bottom': '0',
+        })),
+      ])
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0,
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ]),
+  ]
 })
 export class ListaDeNotasComponent implements OnInit {
 
-  notas: Notas[] = []
+  notas: Notas[] = [];
+  notasFiltradas: Notas[] = [];
+  busca: any;
 
   constructor(private notasService: NotasService) { }
 
   ngOnInit(): void {
     this.notas = this.notasService.buscarTodasNotas();
+    this.notasFiltradas = this.notas;
   }
 
-  deletarNota(id: number){
+  deletarNota(id: number) {
     this.notasService.removerNotas(id);
   }
 
+  buscarNota(query: any) {
+    this.busca = query.target.value;
+    if (this.busca.length < 1) {
+      this.notasFiltradas = this.notas;
+    } else {
+      this.notasFiltradas = this.notas.filter(notas => notas.title == this.busca)
+    }
+  }
 }
